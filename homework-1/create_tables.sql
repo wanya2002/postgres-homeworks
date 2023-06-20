d25601da34f27f9b07b1b3904a6549c6fa666b1e
wanya2002 <wanya2002@yandex.ru>
2023-06-20 11:09:49 +0300

    commit1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE customers
+(
+	customer_id char(5) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE employees
+(
+	employees_id int PRIMARY KEY,
+	first_name varchar(20) NOT NULL,
+	last_name varchar(20) NOT NULL,
+	title varchar(50) NOT NULL,
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id char(5) REFERENCES customers(customer_id) NOT NULL,
+	employees_id int REFERENCES employees(employees_id) NOT NULL,
+	order_date date,
+	ship_city varchar(30)
+)
