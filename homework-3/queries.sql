7b1c58a81877f1f9ab7e17bbb46dd6447ca85451
wanya2002 <wanya2002@yandex.ru>
2023-06-23 09:02:25 +0300

    commit4

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,16 +1,30 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-
+select company_name as customer, CONCAT(first_name, ' ', last_name) as employee from customers
+join orders using(customer_id)
+join employees using(employee_id)
+where employees.city='London' and customers.city='London'
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-
+select product_name, units_in_stock, contact_name, phone from products
+inner join suppliers using(supplier_id)
+inner join categories using(category_id)
+where discontinued=0 and units_in_stock < 25
+and category_name in ('Dairy Products', 'Condiments')
+order by units_in_stock
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-
+select company_name from customers
+full join orders using(customer_id)
+where order_id is null
+order by company_name
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+select product_name from
+products where product_id in (select product_id
+from order_details where quantity=10)
